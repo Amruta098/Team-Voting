b71fef51485b127d0408c5813769458b561a0567
Amruta098 <83020702+Amruta098@users.noreply.github.com>
2021-09-30 18:51:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+  apiKey: "AIzaSyCVa3LJWyzK-cu9jT4T_3Qhe1XWa7dmkBU",
+  authDomain: "project-67-b8680.firebaseapp.com",
+  projectId: "project-67-b8680",
+  storageBucket: "project-67-b8680.appspot.com",
+  messagingSenderId: "725629778097",
+  appId: "1:725629778097:web:16240a85da7bb4cdab4a90"
+}
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
